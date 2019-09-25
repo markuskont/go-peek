3510d593c85d3a5be59b9676f6bc197211281b4a
Markus Kont <markus.kont@ccdcoe.org>
2019-09-25 17:33:05 +0300

    wip
    anomaly detection stuff
    
    Mostly just exploratory coding to see what sticks

--- pkg/statistics/statistics.go
+++ pkg/statistics/statistics.go
@@ -1,9 +1,12 @@
 package statistics
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
 var (
-	OutlierScore = 4.0
+	OutlierScore = 3.0
 )
 
 func init() {
@@ -17,24 +20,60 @@ func init() {
 
 // Standardize scales a value around mean 0 and stddev 1, allows measurements of different scales to be compared
 func Standardize(value, mean, stdDev float64) float64 { return (value - mean) / stdDev }
+func AbsStandardize(v, m, s float64) float64          { return math.Abs(Standardize(v, m, s)) }
 
-// Normalize, or Min-Max Scalar will scale a value to a fixed range, usually 0 to 1
+// Normalize which functions as or Min-Max Scalar will scale a value to a fixed range, usually 0 to 1
 func Normalize(value, min, max float64) float64 { return (value - min) / (max - min) }
 
-func ValueIsOutlier(val, mean, stdDev float64) bool {
-	rng := OutlierScore * stdDev
-	return val > mean-rng || val < mean+rng
+type Values struct {
+	Last, Current float64
+	Max, Min      float64
+	K             float64
+}
+
+func (v *Values) Update(value float64) *Values {
+	v.Last = v.Current
+	v.Current = value
+	if v.Current < v.Min || v.K == 0 {
+		v.Min = value
+	}
+	if v.Current > v.Max || v.K == 0 {
+		v.Max = value
+	}
+	v.K++
+	return v
+}
+
+func (v *Values) Increment() *Values {
+	v.Last = v.Current
+	v.Current++
+	v.K++
+	return v
+}
+
+func (v Values) Delta() float64 {
+	return v.Current - v.Last
+}
+
+func (v Values) Normalize() float64 {
+	return Normalize(v.Current, v.Min, v.Max)
 }
 
 type Measurement struct {
-	Tags             map[string]string
-	Last, Current    float64
-	Rate             float64
-	Total            float64
-	Timestamp, Since time.Time
-	Period           time.Duration
+	Values
+	Tags      map[string]string
+	Rate      float64
+	Timestamp time.Time
+	Gauge     bool
 }
 
-func (m *Measurement) Calculate() *Measurement {
+func (m *Measurement) Update(value float64) *Measurement {
+	m.Timestamp = time.Now()
+	m.Values.Update(value)
+	return m
+}
+
+func (m *Measurement) CalculateRatePerSec(since time.Duration) *Measurement {
+	m.Rate = m.Values.K / since.Seconds()
 	return m
 }
