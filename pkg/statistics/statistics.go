5ce769525a5a992da0594379bdb9bf867775d3d5
Markus Kont <markus.kont@ccdcoe.org>
2019-09-25 12:54:23 +0300

    anomaly detection stuff

--- pkg/statistics/statistics.go
+++ pkg/statistics/statistics.go
@@ -1,1 +1,25 @@
 package statistics
+
+var (
+	OutlierScore = 4.0
+)
+
+func init() {
+	// theoretical rule of thumb - most measurements should fall within 3 standard deviations from mean
+	// no reason to have a threshold lower than that
+	// higher thresh should be used in practice
+	if OutlierScore < 3.0 {
+		OutlierScore = 3.0
+	}
+}
+
+// Standardize scales a value around mean 0 and stddev 1, allows measurements of different scales to be compared
+func Standardize(value, mean, stdDev float64) float64 { return (value - mean) / stdDev }
+
+// Normalize, or Min-Max Scalar will scale a value to a fixed range, usually 0 to 1
+func Normalize(value, min, max float64) float64 { return (value - min) / (max - min) }
+
+func ValueIsOutlier(val, mean, stdDev float64) bool {
+	rng := OutlierScore * stdDev
+	return val > mean-rng || val < mean+rng
+}
