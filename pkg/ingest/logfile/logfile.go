f4613f1ec566648588d53ed5d17f8300fc7814e4
Markus Kont <markus.kont@ccdcoe.org>
2019-10-01 06:35:47 +0300

    wip

--- pkg/ingest/logfile/logfile.go
+++ pkg/ingest/logfile/logfile.go
@@ -2,15 +2,38 @@ package logfile
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/ccdcoe/go-peek/pkg/models/consumer"
+	"github.com/ccdcoe/go-peek/pkg/utils"
 )
 
+type Config struct {
+	Path string
+}
+
+func (c *Config) Validate() error {
+	if c.Path != "" && !utils.StringIsValidDir(c.Path) {
+		return fmt.Errorf("%s is not a valid directory", c.Path)
+	}
+	return nil
+}
+
 type Consumer struct {
-	h      *Handle
+	h      []*Handle
 	tx     chan *consumer.Message
 	ctx    context.Context
 	cancel context.CancelFunc
+}
+
+func NewConsumer(c *Config) (*Consumer, error) {
+	if c == nil {
+		return nil, fmt.Errorf("logfile consumer is missing config")
+	}
+	l := &Consumer{
+		tx: make(chan *consumer.Message),
+	}
+	return l, nil
 }
 
 // Messages implements consumer.Messager
